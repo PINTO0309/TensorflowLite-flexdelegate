6fa15400a182c23f57c6392bdbbfdda3c02119c4
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2019-11-24 00:03:28 +0900

    update

---
+++ log-cleaner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TARGETS=(
+  "*.whl"
+  "*.tar.gz"
+)
+
+target=$(printf " %s" "${TARGETS[@]}")
+target=${target:1}
+
+sudo rm -rf .git/refs/original
+git filter-branch --index-filter "git rm -r --cached --ignore-unmatch ${target}" -- --all
+
