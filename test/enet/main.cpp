c0f15e33b72f9880e71492c835ff5e36367812de
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2019-12-01 07:58:58 +0900

    update

--- test/enet/main.cpp
+++ test/enet/main.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+#include <memory>
+#include <string>
 #include <stdio.h>
+#include <sstream>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 #include "tensorflow/lite/interpreter.h"
 #include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model.h"
 #include "tensorflow/lite/optional_debug_tools.h"
+#include "utils.h"
 
 #define MODEL_FILENAME RESOURCE_DIR"enet.tflite"
 
@@ -17,16 +23,21 @@
 int main()
 {
 	/* read input image data */
-	cv::Mat image = cv::imread(RESOURCE_DIR"test.png");
+	cv::Mat image = cv::imread(RESOURCE_DIR"test.jpg", 1);
 	cv::imshow("InputImage", image);
-	
-	//cv::cvtColor(image, image, CV_BGR2GRAY);
-   //cv::cvtColor(image, image, 6);
+
+   // Initialize Colormap.
+   auto color_map = CreatePascalLabelColormap();
+
+    // Initialize Threads.
+   auto num_of_threads = 4;
+
 	cv::resize(image, image, cv::Size(480, 360));
-	image = ~image;
+	//image = ~image;
 	cv::imshow("InputImage for CNN", image);
 	image.convertTo(image, CV_32FC1, 1.0 / 255);
-
+	//cv::waitKey(0);
+	//return 0;
 	// TFLite
 	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile(MODEL_FILENAME);
 	TFLITE_MINIMAL_CHECK(model != nullptr);
@@ -40,20 +51,46 @@ int main()
 	TFLITE_MINIMAL_CHECK(interpreter != nullptr);
 	printf("=== Interpreter Built ===\n");
 
+	// Get input tensor size
+	const auto& dimensions = interpreter->tensor(interpreter->inputs()[0])->dims;
+  	auto input_array_size = 1;
+	std::vector<int> input_tensor_shape;
+	input_tensor_shape.resize(dimensions->size);
+  	for (auto i = 0; i < dimensions->size; i++)
+  	{
+    	input_tensor_shape[i] = dimensions->data[i];
+    	input_array_size *= input_tensor_shape[i];
+  	}
+  	std::ostringstream input_string_stream;
+  	std::copy(input_tensor_shape.begin(), input_tensor_shape.end(), std::ostream_iterator<int>(input_string_stream, " "));
+  	std::cout << "input shape: " << input_string_stream.str() << std::endl;
+  	std::cout << "input array size: " << input_array_size << std::endl;
+    printf("=== Got model input size ===\n");
+	cv::waitKey(0);
+	//return 0;
+
 	// Allocate tensor buffers.
+    interpreter->SetNumThreads(num_of_threads);
 	TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
 	printf("=== Pre-invoke Interpreter State ===\n");
 	tflite::PrintInterpreterState(interpreter.get());
+	cv::waitKey(0);
+	//return 0;
 
 	// Set data to input tensor
 	float* input = interpreter->typed_input_tensor<float>(0);
-	memcpy(input, image.reshape(0, 1).data, sizeof(float) * 1 * 28 * 28 * 1);
+	//memcpy(input, image.reshape(0, 1).data, sizeof(float) * 1 * 360 * 480 * 3);
+    memcpy(input, image.data, sizeof(float) * input_array_size);
+	printf("\n\n=== MEM copied ===\n");
+	cv::waitKey(0);
 
 	// Run inference
+	printf("\n\n=== Pre-invoke Interpreter State ===\n");
 	TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
 	printf("\n\n=== Post-invoke Interpreter State ===\n");
 	tflite::PrintInterpreterState(interpreter.get());
-
+	cv::waitKey(0);
+	//return 0;
 	// Get data from output tensor
 	float* probs = interpreter->typed_output_tensor<float>(0);
 	for (int i = 0; i < 10; i++) {
